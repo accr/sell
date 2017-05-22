494a66b8f4e7164ed8c619a4bca433cdf7fb1cac
accr <574150795@qq.com>
2017-05-22 19:40:54 +0800

    5.22
    v-router & v-resource

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue';
+import Router from 'vue-router';
+import VueResource from 'vue-resource';
+import goods from '@/components/goods/goods.vue';
+import ratings from '@/components/ratings/ratings.vue';
+import seller from '@/components/seller/seller.vue';
 
-Vue.use(Router)
+import 'common/stylus/index.styl';
+
+Vue.use(Router);
+Vue.use(VueResource);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: goods,
+      component: goods
+    },
+    {
+      path: '/goods',
+      name: 'goods',
+      component: goods
+    },
+    {
+      path: '/ratings',
+      name: 'ratings',
+      component: ratings
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: seller
     }
-  ]
-})
+  ],
+  linkActiveClass: 'active'
+});
